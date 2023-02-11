cb394935eaa4d362c2eeb275649d18b994486573
dim.tutushkin <egiktumane916@gmail.com>
2023-02-11 15:11:00 +0300

    Сделан Todo App

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import "./todo-list-item.css";
+
+export default class TodoListItem extends Component {
+  render() {
+    const {
+      label,
+      onDeleted,
+      onToggleImportant,
+      onToggleDone,
+      important,
+      done,
+    } = this.props;
+
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += " done";
+    }
+
+    if (important) {
+      classNames += " important";
+    }
+
+    return (
+      <span className={classNames}>
+        <span className="todo-list-item-label" onClick={onToggleDone}>
+          {label}
+        </span>
+
+        <button
+          onClick={onToggleImportant}
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+        >
+          <i className="fa fa-exclamation"></i>
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+          onClick={onDeleted}
+        >
+          <i className="fa-solid fa-trash"></i>
+        </button>
+      </span>
+    );
+  }
+}
